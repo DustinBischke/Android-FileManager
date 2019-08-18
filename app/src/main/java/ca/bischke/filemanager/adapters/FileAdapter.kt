f1d86f964acd976ff0536e0df6c8df74a3e86cf8
Dustin <dustin@bischke.ca>
2019-08-18 13:41:35 -0600

    Added File Adapter

--- app/src/main/java/ca/bischke/filemanager/adapters/FileAdapter.kt
+++ app/src/main/java/ca/bischke/filemanager/adapters/FileAdapter.kt
@@ -1,2 +1,29 @@
 package ca.bischke.filemanager.adapters
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ca.bischke.filemanager.R
+import kotlinx.android.synthetic.main.list_file.view.*
+import java.io.File
+
+class FileAdapter(private val fileList: ArrayList<File>) : RecyclerView.Adapter<FileAdapter.FileViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_file, parent, false)
+        return FileViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
+        holder.textFileName.text = fileList.get(position).name
+    }
+
+    override fun getItemCount(): Int {
+        return fileList.size
+    }
+
+    class FileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textFileName: TextView = view.text_file_name
+    }
+}
--- app/src/main/java/ca/bischke/filemanager/fragments/FilesFragment.kt
+++ app/src/main/java/ca/bischke/filemanager/fragments/FilesFragment.kt
@@ -5,13 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.bischke.filemanager.R
+import ca.bischke.filemanager.adapters.FileAdapter
+import kotlinx.android.synthetic.main.fragment_files.*
+import java.io.File
 
 class FilesFragment : Fragment() {
+    private val files: ArrayList<File> = ArrayList()
     private lateinit var mView: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.fragment_files, container, false)!!
         return mView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerview_files.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = FileAdapter(files)
+        }
+
+        files.add(File("Test"))
+        files.add(File("Test2"))
+        files.add(File("Test3"))
     }
 }
